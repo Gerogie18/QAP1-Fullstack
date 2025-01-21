9ceea83a3af5c25709745491ca2cf9cc7cbe2d3b
Gerogie18 <jennlyver@gmail.com>
2025-01-21 00:16:40 -0330

    Create PasswordGenerator.js

---
+++ PasswordGenerator.js
@@ -0,0 +1,17 @@
+// Jennifer Lyver
+// 01/21/2024
+//Description: CLI app that uses flags to generate a random password
+
+// make help flag
+
+//length flag (default to 8)
+
+//flag for lowercase
+
+//flag for upper case
+
+//flag for numbers
+
+//flag for special characters
+
+//default to all - flags to limit
